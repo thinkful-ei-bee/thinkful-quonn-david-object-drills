befd180469baa39fd60b664382aaa0e67efe6cc4
David Bolin <david@bolin.at>
2019-02-21 08:46:45 -0800

    added object updater

--- object-drills1.js
+++ object-drills1.js
@@ -1,13 +1,27 @@
+'use strict';
+
 // Problem #1
 function createMyObject() {
   return {
-    foo: "bar",
+    foo: 'bar',
     answerToUniverse: 42,
-    "olly olly": "oxen free",
+    'olly olly': 'oxen free',
     sayHello: function() {
-      return "hello";
+      return 'hello';
     }
   };
 }
 
-console.log(createMyObject());
+//console.log(createMyObject());
+
+// Problem #2
+function updateObject(obj) {
+  obj.foo = 'foo';
+  obj.bar = 'bar';
+  obj.bizz = 'bizz';
+  obj.bang = 'bang';
+  return obj;
+}
+
+// console.log(updateObject({a: 'xyz', b: 'kjd'}));
+
