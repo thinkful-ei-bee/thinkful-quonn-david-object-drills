8080c8297786b0cdb93339f0b32dd2d6a946cc13
David Bolin <david@bolin.at>
2019-02-21 11:30:04 -0800

    iterating over properties

--- object-drills-extra.js
+++ object-drills-extra.js
@@ -1,11 +1,26 @@
+'use strict';
+
 const loaf = {
   flour: 300,
   water: 210
 };
 
-console.log(loaf.flour, loaf.water);
+// console.log(loaf.flour, loaf.water);
 loaf.hydration = function() {
   return (this.water / this.flour) * 100;
 };
 
-console.log(loaf.hydration());
+// console.log(loaf.hydration());
+
+const weirdObj = {
+  foo: 42,
+  bar: 'xyz',
+  fum: 7,
+  quux: 'abc',
+  spam: 'spam spam spam spam spam'
+};
+
+for (const key in weirdObj) {
+  // console.log(key, weirdObj[key]);
+}
+
