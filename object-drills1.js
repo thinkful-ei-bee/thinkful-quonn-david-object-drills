751673c8f0f4cda4db198f48f6f3f1455423ef3e
Quonn Bernard <qber83@gmail.com>
2019-02-21 08:38:54 -0800

    initial commit/added soltions for first problem

---
+++ object-drills1.js
@@ -0,0 +1,13 @@
+// Problem #1
+function createMyObject() {
+  return {
+    foo: "bar",
+    answerToUniverse: 42,
+    "olly olly": "oxen free",
+    sayHello: function() {
+      return "hello";
+    }
+  };
+}
+
+console.log(createMyObject());
