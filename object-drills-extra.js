07c7e566990cfaad4bb90b14f9ef5407d7266298
David Bolin <david@bolin.at>
2019-02-21 12:47:08 -0800

    finishing LOTR examples

--- object-drills-extra.js
+++ object-drills-extra.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 const loaf = {
   flour: 300,
@@ -14,10 +14,10 @@ loaf.hydration = function() {
 
 const weirdObj = {
   foo: 42,
-  bar: "xyz",
+  bar: 'xyz',
   fum: 7,
-  quux: "abc",
-  spam: "spam spam spam spam spam"
+  quux: 'abc',
+  spam: 'spam spam spam spam spam'
 };
 
 for (const key in weirdObj) {
@@ -26,13 +26,13 @@ for (const key in weirdObj) {
 
 const newObj = {
   meals: [
-    "breakfast",
-    "second breakfast",
-    "elevenses",
-    "lunch",
-    "afternoon tea",
-    "dinner",
-    "supper"
+    'breakfast',
+    'second breakfast',
+    'elevenses',
+    'lunch',
+    'afternoon tea',
+    'dinner',
+    'supper'
   ]
 };
 
@@ -45,12 +45,12 @@ function person(name, jobTitle) {
   };
 }
 
-const person1 = person("Peter", "Software Engineer");
-const person2 = person("Cody", "Owner");
-const person3 = person("Ralph", "Salesman");
+const person1 = person('Peter', 'Software Engineer');
+const person2 = person('Cody', 'Owner');
+const person3 = person('Ralph', 'Salesman');
 
-person1.boss = "Cody";
-person3.boss = "Cody";
+person1.boss = 'Cody';
+person3.boss = 'Cody';
 
 const personsArr = [person1, person2, person3];
 
@@ -71,19 +71,19 @@ const codeKey = {
 
 function decode(word) {
   const letter = word[0];
-  return codeKey[letter] ? word[codeKey[letter] - 1] : " ";
+  return codeKey[letter] ? word[codeKey[letter] - 1] : ' ';
 }
 
 function decodeWords(words) {
-  const arr = words.split(" ");
+  const arr = words.split(' ');
   const newArr = [];
   for (let i = 0; i < arr.length; i++) {
     newArr.push(decode(arr[i]));
   }
-  return newArr.join("");
+  return newArr.join('');
 }
 
-console.log(decodeWords("craft block argon meter bells brown croon droop"));
+console.log(decodeWords('craft block argon meter bells brown croon droop'));
 
 function createCharacter(name, nickname, race, origin, attack, defense) {
   return {
@@ -97,9 +97,9 @@ function createCharacter(name, nickname, race, origin, attack, defense) {
       console.log(`${this.name} is a ${this.race} from ${this.origin}`);
     },
     evaluateFight: function(charcter) {
-      let yourDamage = this.attack - charchter.defense;
+      let yourDamage = this.attack - charcter.defense;
       let thierDamage = charcter.attack - this.defense;
-      if (yourDamge < 0) {
+      if (yourDamage < 0) {
         yourDamage = 0;
       } else if (thierDamage < 0) {
         thierDamage = 0;
@@ -110,17 +110,24 @@ function createCharacter(name, nickname, race, origin, attack, defense) {
 }
 const characters = [
   createCharacter(
-    "Gandalf the White",
-    "gandalf",
-    "Wizard",
-    "Middle Earth",
+    'Gandalf the White',
+    'gandalf',
+    'Wizard',
+    'Middle Earth',
     10,
     6
   ),
-  createCharacter("Bilbo Baggins", "bilbo", "hobbit", "The Shire", 2, 1),
-  createCharacter("Frodo Baggins", "frodo", "hobbit", "The Shire", 3, 2),
-  createCharacter("Aragon son Arathorn", "aragon", "man", "Dunnedain", 6, 8),
-  createCharacter("Legolas", "legolas", "Elf", "Woodland Realm", 8, 5)
+  createCharacter('Bilbo Baggins', 'bilbo', 'Hobbit', 'The Shire', 2, 1),
+  createCharacter('Frodo Baggins', 'frodo', 'Hobbit', 'The Shire', 3, 2),
+  createCharacter('Aragorn son Arathorn', 'aragorn', 'Man', 'Dunnedain', 6, 8),
+  createCharacter('Legolas', 'legolas', 'Elf', 'Woodland Realm', 8, 5)
 ];
 
-console.log(characters);
+characters.push(createCharacter('Arwen Undomiel', 'arwen', 'Half-Elf', 'Rivendell', 7, 7));
+
+characters.find(function(char){return char.nickname === 'aragorn';}).describe();
+
+const hobbits = characters.filter(char => (char.race === 'Hobbit'));
+
+const highdamage = characters.filter(char => (char.attack > 5)); 
+
